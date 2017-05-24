095248d7b36aba268e4b89aa6e933a2603968bf8
Victor Khovanskiy <victor.khovanskiy@gmail.com>
2017-05-24 15:58:45 +0300

    Base arch

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Victor Khovanskiy
  * @since 1.0.0
  */
+@Slf4j
 public class MainApplication {
+    public static void main(String[] args) {
+        log.info("");
+    }
 }
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,17 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Victor Khovanskiy
+ * @since 1.0.0
+ */
+@Slf4j
+public class BaseTest {
+
+    @Test
+    public void main() {
+        log.info("Base Test");
+        Assert.assertTrue("Dummy test", 1 == 1);
+    }
+}
